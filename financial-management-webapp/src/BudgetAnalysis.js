a324aa62f2f9517b840acbe5bb56a7f041a8e52c
cherylchiaa <cchi0074@student.monash.edu>
2024-08-12 23:19:13 +1000

    monthly income & expected saving on budget analysis page

--- financial-management-webapp/src/BudgetAnalysis.js
+++ financial-management-webapp/src/BudgetAnalysis.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
+function SaveButton() {
+  return (
+    <button>
+      Save
+    </button>
+  );
+}
+
 function BudgetAnalysisPage() {
   return (
     <div>
       <h1>Budget Analysis Page</h1>
+      <p>Monthly Income: <input type="text" placeholder="Enter a number" /></p>
+      <p>Expected Savings: <input type="text" placeholder="Enter a number" /></p>
+      <SaveButton />
     </div>
   );
 }
 
 export default BudgetAnalysisPage;
+
+
 
